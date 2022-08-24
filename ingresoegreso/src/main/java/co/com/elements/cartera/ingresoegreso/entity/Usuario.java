0c5e02c2642dad8c6d910423d65ed49abec0e1c4
Alvaro Uribe <adummis@hotmail.com>
2022-08-24 16:43:59 -0500

    Subiendo proyecto base

--- ingresoegreso/src/main/java/co/com/elements/cartera/ingresoegreso/entity/Usuario.java
+++ ingresoegreso/src/main/java/co/com/elements/cartera/ingresoegreso/entity/Usuario.java
@@ -1,2 +1,100 @@
-package co.com.elements.cartera.ingresoegreso.entity;public class Usuario {
+package co.com.elements.cartera.ingresoegreso.entity;
+
+public class Usuario {
+    private long idUsuario;
+    private String nombre;
+    private String apellido;
+    private TipoDocumento tipoDocumento;
+    private String cedula;
+    private String usuario;
+    private String clave;
+    private Rol rol;
+    private boolean estado;
+
+    public long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUsuario=" + idUsuario +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", tipoDocumento=" + tipoDocumento +
+                ", cedula='" + cedula + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", clave='" + clave + '\'' +
+                ", rol=" + rol +
+                ", estado=" + estado +
+                '}';
+    }
 }
